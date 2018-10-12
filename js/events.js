569fbe93f7c34c6f157d2c109cce50ed17ba1d3d
Jiamin Lim <jiaminlim93@gmail.com>
2018-10-12 17:56:28 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,27 @@
 //define functions here
+function getIt(){
+  $('p').on('click',(e)=>{alert('Hey!')})
+}
+
+function frameIt(){
+  $('img').on('load',(e)=>$('img').append('.tasty'))
+}
+
+function pressIt(){
+  $('input').on('keydown',(e)=>{
+    if(e.key==='g'){
+      alert('You have pressed "g"!')
+    }
+  })
+}
+function submitIt(){
+  $('form').on('submit',(e)=>{alert('Your form is going to be submitted now.')})
+}
 
 $(document).ready(function(){
-
+getIt()
+frameIt()
+pressIt()
+submitIt()
 // call functions here
-
 });

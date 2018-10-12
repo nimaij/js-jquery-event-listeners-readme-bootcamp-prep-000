a9eaa14750e858ebf169b06a04c00a7e4b82b1fa
Jiamin Lim <jiaminlim93@gmail.com>
2018-10-12 18:08:50 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -8,7 +8,7 @@ function frameIt(){
 }
 
 function pressIt(){
-  $('#typing').on('keydown',(e)=>{
+  $('input#typing').on('keydown',(e)=>{
     if(e.key==='G'){
       alert('You have pressed "G"!')
     }
